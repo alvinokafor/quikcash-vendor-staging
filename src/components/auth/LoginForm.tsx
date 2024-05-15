df1eacbc2903a99a1fa254f0df47a4f58be593b6
Alvin Okafor <alvin2k99@gmail.com>
2024-05-15 16:42:25 +0100

    otp fix

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -77,6 +77,25 @@ export default function LoginForm() {
   //   }
   // };
 
+  const handleSendOTP = async () => {
+    try {
+      await axios.post(
+        `http://165.227.77.33:5001/api/initiate-otp/`,
+        {
+          email: watchFields[0],
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+          },
+        }
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   const handleLogin = () => {
     if (!watchFields[0] || !watchFields[1]) {
       toast({
@@ -104,6 +123,7 @@ export default function LoginForm() {
         localStorage.setItem("accessToken", accesstoken);
         localStorage.setItem("user", JSON.stringify(user));
         setIsPending(false);
+        handleSendOTP();
         user.two_factor_enabled
           ? toast({
               position: "top-right",
